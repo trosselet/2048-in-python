21f4b20d1d108b231be58fa596c412b53eb03aeb
trosselet <trosselet@gaming.tech>
2023-12-04 12:02:06 +0100

    CORRECT the movements

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def GetBoard() -> list[list[str]]:
     return board
 
 #Verify if Inputs is in authorized_inputs
-def ask_input(message: str, authorized_inputs: list) -> str:
+def asj_input(message: str, authorized_inputs: list) -> str:
     while True :
         answer: str = input(message + str(authorized_inputs) + " : ")
         
@@ -24,7 +24,7 @@ def ask_input(message: str, authorized_inputs: list) -> str:
 
 def show_board(board: list[list[str]]) -> None:
     for row in board:
-        print(" ".join(str(sign) for sign in row))
+        print("    ".join(str(sign) for sign in row))
 
 def spawn_tile(board: list[list[str]]) -> tuple[int,int]:
     empty_spots: list[tuple[int, int]] = [(i, j) for i in range(len(board)) for j in range(len(board)) if board[i][j] == "."]
@@ -38,54 +38,68 @@ def spawn_tile(board: list[list[str]]) -> tuple[int,int]:
 def full_board(board: list[list[str]]) -> bool:
     for row in board:
         for sign in row:
-            if sign is ".":
-                return False
-    return True
+            if sign == ".":
+                return False  # S'il y a une case vide, la grille n'est pas pleine
+    return True  # Si aucune case vide n'est trouvée, la grille est pleine
 
 def movement (board) -> str:
     dico_movement : dict = {"up" : "u", "right" : "r", "down" : "d", "left" : "l" }
-    direction: str = ask_input("Entrez une direction" ,(list(dico_movement.values()) + list(dico_movement.keys())))
-    if direction == "up" or "u" :
-        for i in range(1, BOARD_LENGTH):
-            for j in range(BOARD_LENGTH) :
+    direction: str = asj_input("Entrez une direction" ,(list(dico_movement.values()) + list(dico_movement.keys())))
+    if direction == "up" or direction == "u":
+        for j in range(BOARD_LENGTH):
+            for i in range(1, BOARD_LENGTH):
                 if board[i][j] != ".":
-                    if board[i][j] == board[i-1][j] :
-                        board[i-1][j] = str(int(board[i-1][j])*2)
-                    else :
-                        board[i-1][j] = board[i][j]
-                    board[i][j] = "."
+                    while i > 0 and (board[i - 1][j] == "." or board[i - 1][j] == board[i][j]):
+                        if board[i - 1][j] == ".":
+                            board[i - 1][j] = board[i][j]
+                            board[i][j] = "."
+                        elif board[i - 1][j] == board[i][j]:
+                            board[i - 1][j] = str(int(board[i - 1][j]) * 2)
+                            board[i][j] = "."
+                            break
+                        i -= 1
     
-    if direction == "right" or "r" :
+    if direction == "right" or direction == "r":
         for i in range(BOARD_LENGTH):
-            for j in range(BOARD_LENGTH-1) :
-                j = BOARD_LENGTH - j - 2
+            for j in range(BOARD_LENGTH - 2, -1, -1):
                 if board[i][j] != ".":
-                    if board[i][j] == board[i][j+1] :
-                        board[i][j+1] = str(int(board[i][j+1])*2)
-                    else :
-                        board[i][j+1] = board[i][j]
-                    board[i][j] = "."
+                    while j < BOARD_LENGTH - 1 and (board[i][j + 1] == "." or board[i][j + 1] == board[i][j]):
+                        if board[i][j + 1] == ".":
+                            board[i][j + 1] = board[i][j]
+                            board[i][j] = "."
+                        elif board[i][j + 1] == board[i][j]:
+                            board[i][j + 1] = str(int(board[i][j + 1]) * 2)
+                            board[i][j] = "."
+                            break
+                        j += 1
 
-    if direction == "down" or "d" :
-        for i in range(BOARD_LENGTH-1):
-            i = BOARD_LENGTH - i - 2
-            for j in range(BOARD_LENGTH) :
+    if direction == "down" or direction == "d":
+        for j in range(BOARD_LENGTH):
+            for i in range(BOARD_LENGTH - 2, -1, -1):
                 if board[i][j] != ".":
-                    if board[i][j] == board[i+1][j] :
-                        board[i+1][j] = str(int(board[i+1][j])*2)
-                    else :
-                        board[i+1][j] = board[i][j]
-                    board[i][j] = "."
+                    while i < BOARD_LENGTH - 1 and (board[i + 1][j] == "." or board[i + 1][j] == board[i][j]):
+                        if board[i + 1][j] == ".":
+                            board[i + 1][j] = board[i][j]
+                            board[i][j] = "."
+                        elif board[i + 1][j] == board[i][j]:
+                            board[i + 1][j] = str(int(board[i + 1][j]) * 2)
+                            board[i][j] = "."
+                            break
+                        i += 1
 
-    if direction == "left" or "l" :
+    if direction == "left" or direction == "l":
         for i in range(BOARD_LENGTH):
-            for j in range(1,BOARD_LENGTH) :
+            for j in range(1, BOARD_LENGTH):
                 if board[i][j] != ".":
-                    if board[i][j] == board[i][j-1] :
-                        board[i][j-1] = str(int(board[i][j-1])*2)
-                    else :
-                        board[i][j-1] = board[i][j]
-                    board[i][j] = "."
+                    while j > 0 and (board[i][j - 1] == "." or board[i][j - 1] == board[i][j]):
+                        if board[i][j - 1] == ".":
+                            board[i][j - 1] = board[i][j]
+                            board[i][j] = "."
+                        elif board[i][j - 1] == board[i][j]:
+                            board[i][j - 1] = str(int(board[i][j - 1]) * 2)
+                            board[i][j] = "."
+                            break
+                        j -= 1
 
 def game():
     board = GetBoard()
@@ -94,9 +108,11 @@ def game():
 
         if full_board(board):
             print("Vous avez perdu")
+            show_board(board)
             break
         
         spawn_tile(board)
         show_board(board)
         movement(board)
+        show_board(board)
 game()
